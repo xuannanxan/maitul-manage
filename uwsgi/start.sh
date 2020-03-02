31dc7713355ddfb852bdb08febfc511e49324c6b
xuannanxan <382933169@qq.com>
2020-03-02 18:21:01 +0800

    新增sh

---
+++ uwsgi/start.sh
@@ -0,0 +1,9 @@
+###
+# @Description: 
+# @Author: Xuannan
+# @Date: 2020-03-02 18:18:46
+# @LastEditTime: 2020-03-02 18:20:38
+# @LastEditors: Xuannan
+###
+#!/bin/bash
+/root/.local/share/virtualenvs/service-Hwb9L28H/bin/uwsgi  --ini  /srv/www/maitul-manage/uwsgi/uwsgi.ini
