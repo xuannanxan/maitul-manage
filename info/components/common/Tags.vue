35ddd2946bf4c97806bb38057a7dc9d6fa97c118
xuannanxan <382933169@qq.com>
2020-04-14 00:07:19 +0800

    tags url

--- info/components/common/Tags.vue
+++ info/components/common/Tags.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Xuannan
  * @Date: 2020-02-16 10:31:22
- * @LastEditTime: 2020-03-29 10:13:40
+ * @LastEditTime: 2020-04-14 00:05:25
  * @LastEditors: Xuannan
  -->
 <template>
@@ -13,7 +13,7 @@
             :key="tag" 
             :color="tagColor[Math.floor((Math.random()*tagColor.length))]"
             >
-                <nuxt-link :to="{path:url+'?tag='+tag}">{{tag}}</nuxt-link>
+                <nuxt-link :to="{path:'/'+locale+url+'?tag='+tag}">{{tag}}</nuxt-link>
             </a-tag>
         </div>
 </template>
@@ -22,6 +22,7 @@
     export default {
         data() {
             return {
+                locale:this.$i18n.locale,
                 tagColor:['magenta','red','volcano','orange','gold','lime','green','cyan','blue','geekblue','purple'],
             }
         },
